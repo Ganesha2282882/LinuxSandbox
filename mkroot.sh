345228c2f6bf7772d4a224ba223261c9d58ebc92
Brahma Sharma <59745725+Ganesha2282882@users.noreply.github.com>
2021-02-11 13:36:21 -0500

    Update mkroot.sh

--- mkroot.sh
+++ mkroot.sh
@@ -6,7 +6,9 @@ echo "fetching busybox from busybox.net"
 wget https://busybox.net/downloads/binaries/1.26.2-i686/busybox -q --show-progress -O bb
 }
 getbusybox
-mkdir -p $1/{bin,etc,sbin,usr,usr/sbin}
+mkdir -p $1/{bin,etc,sbin,usr}
+mkdir -p $1/usr/sbin
+mkdir -p $1/usr/
 cp -f bb $1/bin/busybox
 rm bb -f
 cd $1/bin
